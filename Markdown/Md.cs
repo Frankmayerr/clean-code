8c56c8d0ed835ea915c6c8719084cd526f8216d7
Gladkix <y.gladkix@mail.ru>
2017-11-11 20:30:14 +0500

    Практика на паре

--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -1,4 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq.Expressions;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Markdown
 {
@@ -6,7 +15,7 @@ namespace Markdown
 	{
 		public string RenderToHtml(string markdown)
 		{
-			return markdown; //TODO
+			return markdown;
 		}
 	}
 
